9978f939684576a908f762735267b7c1317b25c8
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-09-04 15:55:22 -0600

    finished file structure, initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Footer, Header, Navigation, Project } from './components'
+
+function App() {
+  return (
+    <div className="portfolio-app">
+      <BucketList />
+    </div>
+  );
+}
+
+export default App;
